c56cebc7630f20d2e1997c5bf27739541ce91fa7
Felix <felix.clemens.gail@gmail.com>
2017-07-19 13:02:55 +0200

    Change NetworkException to be able to carry Request
    and create printable output in case a request was added.
    Appen Request in Errorinterceptor

--- src/main/java/com/github/felixgail/gplaymusic/api/exceptions/NetworkException.java
+++ src/main/java/com/github/felixgail/gplaymusic/api/exceptions/NetworkException.java
@@ -5,53 +5,40 @@ import com.google.gson.JsonSyntaxException;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 import okhttp3.Call;
+import okhttp3.Request;
 import okhttp3.Response;
 
 import java.io.IOException;
-import java.io.Reader;
 import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Will be thrown when the network responds with an error.
  * It will contain the error code and (if available) a message.
  */
-public class NetworkException extends IOException implements Serializable{
+public class NetworkException extends IOException implements Serializable {
 
-    private class ErrorHelper implements Serializable{
-        @Expose
-        private int code;
-        @Expose
-        private String message;
-
-        public int getCode() {
-            return code;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-
-        public void setCode(int code) {
-            this.code = code;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-    }
-
+    private final static Gson gson = new Gson();
     @Expose
     @SerializedName("error")
     private ErrorHelper helper = new ErrorHelper();
 
-    private Call call;
+    private Request request;
 
-    public NetworkException(int code, String message){
+    public NetworkException(int code, String message) {
         helper = new ErrorHelper();
         helper.setCode(code);
         helper.setMessage(message);
     }
 
+    public static NetworkException parse(Response response) {
+        try {
+            return gson.fromJson(response.body().charStream(), NetworkException.class);
+        } catch (JsonSyntaxException e) {
+            return new NetworkException(response.code(), response.message());
+        }
+    }
 
     public int getCode() {
         return helper.getCode();
@@ -61,25 +48,62 @@ public class NetworkException extends IOException implements Serializable{
         return helper.getMessage();
     }
 
-    public Call getCall() {
-        return call;
+    public Request getRequest() {
+        return request;
     }
 
-    public void setCall(Call call) {
-        this.call = call;
+    public void setRequest(Request request) {
+        this.request = request;
+    }
+
+    private String getRequestInformation() {
+        if (request != null) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("More Request Information:\n")
+                    .append("URL: ").append(request.url()).append(System.lineSeparator());
+            for (Map.Entry<String, List<String>> entry :
+                    request.headers().toMultimap().entrySet()){
+                sb.append(entry.getKey()).append(": ");
+                for (String value : entry.getValue()) {
+                    sb.append("\t");
+                    if (value.startsWith("GoogleLogin")) {
+                        sb.append("<ommited>");
+                    } else {
+                        sb.append(value);
+                    }
+                    sb.append(System.lineSeparator());
+                }
+            }
+            return sb.toString();
+        }
+        return "";
     }
 
     @Override
     public String toString() {
-        return String.format("%d: %s", getCode(), getMessage());
+        return String.format("%d: %s\n\n%s", getCode(), getMessage(), getRequestInformation());
     }
 
-    private final static Gson gson = new Gson();
-    public static NetworkException parse(Response response) {
-        try {
-            return gson.fromJson(response.body().charStream(), NetworkException.class);
-        } catch (JsonSyntaxException e) {
-            return new NetworkException(response.code(), response.message());
+    private class ErrorHelper implements Serializable {
+        @Expose
+        private int code;
+        @Expose
+        private String message;
+
+        public int getCode() {
+            return code;
+        }
+
+        public void setCode(int code) {
+            this.code = code;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
         }
     }
 }
--- src/main/java/com/github/felixgail/gplaymusic/util/interceptor/ErrorInterceptor.java
+++ src/main/java/com/github/felixgail/gplaymusic/util/interceptor/ErrorInterceptor.java
@@ -3,19 +3,16 @@ package com.github.felixgail.gplaymusic.util.interceptor;
 import com.github.felixgail.gplaymusic.api.exceptions.NetworkException;
 import com.google.gson.Gson;
 import okhttp3.Interceptor;
+import okhttp3.Request;
 import okhttp3.Response;
 
 import java.io.IOException;
 import java.util.logging.Logger;
 
-public class ErrorInterceptor implements Interceptor{
+public class ErrorInterceptor implements Interceptor {
     private static final Logger logger = Logger.getLogger(ErrorInterceptor.class.getName());
     private Gson gson;
     private InterceptorBehaviour behaviour;
-
-    public enum InterceptorBehaviour {
-        LOG, THROW_EXCEPTION
-    }
 
     public ErrorInterceptor(InterceptorBehaviour b) {
         this.behaviour = b;
@@ -24,9 +21,11 @@ public class ErrorInterceptor implements Interceptor{
 
     @Override
     public Response intercept(Chain chain) throws IOException {
-        Response response = chain.proceed(chain.request());
+        Request request = chain.request();
+        Response response = chain.proceed(request);
         if (response.code() >= 400) {
             NetworkException networkException = NetworkException.parse(response);
+            networkException.setRequest(request);
             if (behaviour == InterceptorBehaviour.THROW_EXCEPTION) {
                 throw networkException;
             } else {
@@ -34,5 +33,9 @@ public class ErrorInterceptor implements Interceptor{
             }
         }
         return response;
+    }
+
+    public enum InterceptorBehaviour {
+        LOG, THROW_EXCEPTION
     }
 }
