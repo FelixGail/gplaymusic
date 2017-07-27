5a57aceeeb88690b1bb356df626f8e2205d2f878
Felix <felix.clemens.gail@gmail.com>
2017-07-28 01:26:19 +0200

    Add TokenProvider.provideToken(String)

--- src/main/java/com/github/felixgail/gplaymusic/api/TokenProvider.java
+++ src/main/java/com/github/felixgail/gplaymusic/api/TokenProvider.java
@@ -1,5 +1,6 @@
 package com.github.felixgail.gplaymusic.api;
 
+import com.sun.istack.internal.NotNull;
 import okhttp3.OkHttpClient;
 import svarzee.gps.gpsoauth.AuthToken;
 import svarzee.gps.gpsoauth.Gpsoauth;
@@ -21,13 +22,18 @@ public class TokenProvider {
      * @throws IOException                 on network or connectivity issues
      * @throws Gpsoauth.TokenRequestFailed on invalid credentials
      */
-    public static AuthToken provideToken(final String user, final String password, final String androidID)
+    public static AuthToken provideToken(@NotNull final String user, @NotNull final String password,
+                                         @NotNull final String androidID)
             throws IOException, Gpsoauth.TokenRequestFailed {
         OkHttpClient client = new OkHttpClient();
         Gpsoauth auth = new Gpsoauth(client);
         // clientSig taken from https://github.com/simon-weber/gmusicapi/blob/develop/gmusicapi/session.py#L199
         return auth.login(user, password, androidID, "sj", "com.google.android.music",
                 "AIzaSyARTC1h-_puqO0PHCHUoj1BTDjuAOxNVA8");
+    }
+
+    public static AuthToken provideToken(@NotNull final String token) {
+        return new AuthToken(token);
     }
 
 
