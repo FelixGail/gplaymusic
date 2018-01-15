b6b10bd564b2b1b21d74fd2b15a608ce0464191b
Felix Gail <felix.clemens.gail@gmail.com>
2018-01-15 20:48:59 +0100

    complete test

--- src/test/java/com/github/felixgail/gplaymusic/ArtistTest.java
+++ src/test/java/com/github/felixgail/gplaymusic/ArtistTest.java
@@ -1,6 +1,8 @@
 package com.github.felixgail.gplaymusic;
 
 import com.github.felixgail.gplaymusic.model.Artist;
+import com.github.felixgail.gplaymusic.model.enums.ResultType;
+import com.github.felixgail.gplaymusic.model.requests.SearchTypes;
 import com.github.felixgail.gplaymusic.util.TestUtil;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -8,6 +10,7 @@ import org.junit.Test;
 import svarzee.gps.gpsoauth.Gpsoauth;
 
 import java.io.IOException;
+import java.util.List;
 
 import static com.github.felixgail.gplaymusic.TestWithLogin.getApi;
 import static com.github.felixgail.gplaymusic.TestWithLogin.loginToService;
@@ -21,10 +24,24 @@ public class ArtistTest {
 
   @Test
   public void testRelatedArtists() throws IOException {
-    Artist artist = getApi().getArtist(TestUtil.get("test.artist").get(), false, 0, 10);
-    Assert.assertNotNull(artist);
-    Assert.assertTrue(artist.getRelatedArtists().isPresent());
-    Assert.assertNotEquals(0, artist.getRelatedArtists().get().size());
+    List<Artist> artistList = getApi().search("John", new SearchTypes(ResultType.ARTIST)).getArtists();
+    Assert.assertNotNull(artistList);
+    Assert.assertNotEquals(0, artistList.size());
+    int relArtistNum = 0;
+    for(Artist artist : artistList) {
+      if(artist.getArtistId().isPresent()) {
+        artist = getApi().getArtist(artist.getArtistId().get(), false, 0, 10);
+        Assert.assertNotNull(artist);
+        if(artist.getRelatedArtists().isPresent()){
+          relArtistNum += 1;
+          Assert.assertNotEquals(0, artist.getRelatedArtists().get().size());
+        }
+      }
+    }
+    Assert.assertNotEquals(
+        String.format("No Artist (of %d) had a list of related artists", artistList.size()), 0,
+        relArtistNum);
+    System.out.printf("%d of %d artists had a list of related artists\n", relArtistNum, artistList.size());
   }
 
 
