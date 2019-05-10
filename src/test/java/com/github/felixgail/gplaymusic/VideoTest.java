48a493df17cd769576b3d6a411d99b32a5dcd9d5
Felix Gail <felix.clemens.gail@gmail.com>
2019-05-10 16:08:44 +0200

    Bugfix/video (#59)
    * update vulnerable dependencies
    
    * force okhttp3:3.12.0 for retrofit, remove unused logging-interceptor
    
    * use httpurl instead of string
    
    * improve video test
    
    * bump version

--- src/test/java/com/github/felixgail/gplaymusic/VideoTest.java
+++ src/test/java/com/github/felixgail/gplaymusic/VideoTest.java
@@ -1,5 +1,6 @@
 package com.github.felixgail.gplaymusic;
 
+import com.github.felixgail.gplaymusic.model.Track;
 import com.github.felixgail.gplaymusic.model.Video;
 import com.github.felixgail.gplaymusic.model.requests.SearchTypes;
 import com.github.felixgail.gplaymusic.util.TestUtil;
@@ -18,18 +19,32 @@ public class VideoTest extends TestWithLogin {
     loginToService(TestUtil.USERNAME, TestUtil.PASSWORD, TestUtil.ANDROID_ID, TestUtil.TOKEN);
   }
 
-  //Feature seems to be desabled at the moment.
   @Test
   public void testVideo() throws IOException {
+    //Create a query that should contain video elements
     List<Video> videos = getApi()
-        .search("", new SearchTypes(Video.RESULT_TYPE)).getVideos();
+        .search("All or Nothing Theory", new SearchTypes(Video.RESULT_TYPE)).getVideos();
     TestUtil.assume(videos);
-    Assume.assumeTrue("List of videos is empty.", videos.size() > 0);
-    System.out.printf("Empty search for videos yielded %d results\n", videos.size());
+    Assert.assertTrue("List of videos is empty.", videos.size() > 0);
+    System.out.printf("search for videos yielded %d results\n", videos.size());
     videos.forEach(v -> {
       Assert.assertNotNull("Video is null", v);
       Assert.assertNotNull("ID is null", v.getId());
       Assert.assertTrue("ID is empty", !v.getId().isEmpty());
     });
+  }
+
+  @Test
+  public void testVideoInTrack() throws IOException {
+    List<Track> tracks = getApi()
+            .search("All or Nothing Theory", new SearchTypes(Track.RESULT_TYPE)).getTracks();
+    Assume.assumeFalse("List of tracks is empty", tracks.isEmpty());
+    boolean foundVideo = false;
+    for(Track track : tracks) {
+      if (track.getVideo().isPresent()) {
+        foundVideo = true;
+      }
+    }
+    Assert.assertTrue("No track element contained a video attribute", foundVideo);
   }
 }
